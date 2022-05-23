48682493c9b2bb919062b89c12307b9baad151c5
Ragib Shariar <srragib@gmail.com>
2022-05-24 03:28:49 +0600

    dot env file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
@@ -10,6 +11,31 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5m6szug.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        // console.log('DB Connected');
+        const productCollection = client.db("cherrymx").collection("products");
+
+        // get all products => localhost:5000/products
+        app.get('/products', async(req, res)=>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
