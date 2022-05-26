3df0f69de93e7ad6802ff97bc37f75a9080990ec
Ragib Shariar <srragib@gmail.com>
2022-05-26 23:16:53 +0600

    minor changes

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 // Database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5m6szug.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // JWT token 
